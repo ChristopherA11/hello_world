dd1f6daf0e556ead2f912e6c7a8e06bc3099c514
ChristopherA11 <christopher.a@thydreamtech.com>
2023-11-21 18:04:51 +0530

    add the useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
 
 function App() {
   const price = 50;
   const [qty,setQty] = useState(1)
+  useEffect(() => {
+
+  },[])
 
   const onClick = () => {
-    setQty (qty +1)
+    setQty (qty + 1)
   }
+ 
+  // const handleClick =() => {
+  //   setQty (qty - 1)
+  // }
   return (
     <div className="App">
       <header className="App-header">
-       <div>price:{price}</div>
-       <div>qty: {qty}</div>
-       <div>total:{qty * price}</div>
-       
-       <button onClick={onClick}>Increment</button>
+      
+      <div>price: {price}</div>
+      <div>qty: {qty}</div>
+      <div>total: {qty * price}</div><br/>
+       <button onClick={onClick}>Increment</button><br/>
+     
       </header>
     </div>
   );
